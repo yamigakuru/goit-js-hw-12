e3c8a705ffa3603292a1c0809de3cea1f04b6c14
yamigakuru <qforceplay@gmail.com>
2024-05-27 12:19:05 +0200

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: 'goit-js-hw-12',
     build: {
       sourcemap: true,
 
